6154104109e775c39fdc14657439b69692a4a153
drhawktuah <jrbgnuiojnebwrugnbwr@proton.me>
2025-08-31 09:24:22 +0100

    got bored

---
+++ PortScanner.Core/PortScannerArguments.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.Linq;
+using System.Threading.Tasks;
+using PortScanner.PortScanner.Config;
+
+namespace PortScanner.PortScanner.Core;
+
+public class PortScannerArguments
+{
+    Arg
+}
